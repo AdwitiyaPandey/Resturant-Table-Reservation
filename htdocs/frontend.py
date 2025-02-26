f773b416896b105f432e81c1c00b6f2281c26118
Adwitiya Pandey <adwitiya00.pandey@gmail.com>
2025-02-26 13:07:59 +0545

    Update and rename 1.py to frontend.py

--- htdocs/frontend.py
+++ htdocs/frontend.py
@@ -7,7 +7,6 @@ Restaurant Table Booking System - GUI
 This script creates a Tkinter-based graphical user interface (GUI) for users to book tables at a restaurant.
 
 Features:
-- Background image for UI aesthetics
 - Labels and input fields for user details (Name, Contact, Location, Date, Time, Guests)
 - Dropdown selection for time slots
 - Basic UI setup using Tkinter and ttk widgets
@@ -20,7 +19,7 @@ Date: [2025-02-11]
 root = Tk()
 root.title("Restaurant Table Booking System")
 root.iconbitmap("table.ico")
-root.geometry("600x400")
+root.geometry("800x400")
 root.resizable(0, 0)
 image_path = r"C:\Users\HP Victus\OneDrive\Desktop\tikinterpyhton\725c86e86ca1f893f506c1fbe0b37359.jpg"
 image = Image.open(image_path)
@@ -36,12 +35,12 @@ time_dropdown =ttk.Combobox(root, values=["12:00 PM", "12:30 PM", "1:00 PM", "1:
 time_dropdown.place(x=420, y=210)
 
 Label(root, text="Dine Reserve", font="Lobster 15 bold",bg= "Dark cyan").pack()
-Label(text="Name", font="Garamond 10 bold",bg= "Dark cyan").place(x=288, y=50)
-Label(text="Contact", font="Garamond 10 bold",bg= "Dark cyan").place(x=288, y=90)
-Label(text="Location, Restaurant", font="Garamond 10 bold",bg= "Dark cyan").place(x=288, y=130)
-Label(text="Date (YYYY-MM-DD)", font="Garamond 10 bold",bg= "Dark cyan").place(x=288, y=170)
-Label(text="Time", font="Garamond 10 bold",bg= "Dark cyan").place(x=288, y=210)
-Label(text="Guests", font="Garamond 10 bold",bg= "Dark cyan").place(x=288, y=250)
+Label(text="Name", font="Garamond 15 bold",bg= "Dark cyan").place(x=288, y=50)
+Label(text="Contact", font="Garamond 15 bold",bg= "Dark cyan").place(x=288, y=90)
+Label(text="Location, Restaurant", font="Garamond 15 bold",bg= "Dark cyan").place(x=288, y=130)
+Label(text="Date (YYYY-MM-DD)", font="Garamond 15 bold",bg= "Dark cyan").place(x=288, y=170)
+Label(text="Time", font="Garamond 15 bold",bg= "Dark cyan").place(x=288, y=210)
+Label(text="Guests", font="Garamond 15 bold",bg= "Dark cyan").place(x=288, y=250)
 
 e1 = Entry(root) 
 e1.place(x=420, y=50)
